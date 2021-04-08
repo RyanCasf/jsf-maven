bf5a2919ae1a541146050f67d11f5869d31cff32
RyanCasf <ryaninternet62@gmail.com>
2021-04-07 23:57:28 -0300

    Criação de 'login' em Servlet puro Listening request to response.

---
+++ Servlet/src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,37 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/servlet/*")
+public class HelloServlet extends HttpServlet 
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+    {
+        resp.getWriter().print("Seja bem-vindo!");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+    {
+        String nomeUsuario = req.getParameter("nome");
+        nomeUsuario = (!nomeUsuario.isEmpty()) ? nomeUsuario : "Novo usuário";
+
+        StringBuilder html = new StringBuilder();
+        html.append("<html>");
+        html.append(" <head>");
+        html.append("  <title>Estudando Servlets - Recuperando dados enviados por parâmetro</title>");
+        html.append(" </head>");
+        html.append(" <body>");
+        html.append("  <h1>Seja bem-vindo, " + nomeUsuario + "!</h1>");
+        html.append(" </body>");
+        html.append("</html>");
+
+        resp.getWriter().print(html);
+    }
+}

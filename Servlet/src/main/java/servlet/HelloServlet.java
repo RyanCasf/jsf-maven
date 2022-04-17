6f6084ce42cc36df1b172012316b07e530b3e6e8
= <ryaninternet62@gmail.com>
2022-04-16 23:57:13 -0300

    Refactoring JSF e update

--- Servlet/src/main/java/servlet/HelloServlet.java
+++ Servlet/src/main/java/servlet/HelloServlet.java
@@ -1,39 +1,42 @@
 package servlet;
 
-import javax.servlet.ServletException;
+import java.io.IOException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet("/servlet/*")
-public class HelloServlet extends HttpServlet 
-{
-	private static final long serialVersionUID = 6747677077926573259L;
+public class HelloServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+		try {
+			resp.getWriter().print("Seja bem-vindo!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
-    {
-        resp.getWriter().print("Seja bem-vindo!");
-    }
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+		try {
+			String nomeUsuario = req.getParameter("nome");
+			nomeUsuario = (!nomeUsuario.isEmpty()) ? nomeUsuario : "Novo Usu·rio!";
 
-    @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
-    {
-        String nomeUsuario = req.getParameter("nome");
-        nomeUsuario = (!nomeUsuario.isEmpty()) ? nomeUsuario : "Novo usu√°rio";
+			StringBuilder html = new StringBuilder();
+			html.append("<html>");
+			html.append(" <head>");
+			html.append("  <title>Estudando Servlets - Recuperando dados enviados por par‚metro</title>");
+			html.append(" </head>");
+			html.append(" <body>");
+			html.append("  <h1>Seja bem-vindo, " + nomeUsuario + "!</h1>");
+			html.append(" </body>");
+			html.append("</html>");
 
-        StringBuilder html = new StringBuilder();
-        html.append("<html>");
-        html.append(" <head>");
-        html.append("  <title>Estudando Servlets - Recuperando dados enviados por par√¢metro</title>");
-        html.append(" </head>");
-        html.append(" <body>");
-        html.append("  <h1>Seja bem-vindo, " + nomeUsuario + "!</h1>");
-        html.append(" </body>");
-        html.append("</html>");
-
-        resp.getWriter().print(html);
-    }
+			resp.getWriter().print(html);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }

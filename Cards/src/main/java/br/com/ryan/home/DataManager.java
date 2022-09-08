71bf43176339e01d3b8ea02f11c4e5e60da7a9fd
= <ryaninternet62@gmail.com>
2022-09-08 08:54:56 -0300

    Adicionei a data básica.

---
+++ Cards/src/main/java/br/com/ryan/home/DataManager.java
@@ -0,0 +1,22 @@
+package br.com.ryan.home;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean @ViewScoped
+public class DataManager implements Serializable {
+	private static final long serialVersionUID = 4465136805007413709L;
+
+	private Date dataPadrao;
+	
+	public Date getDataPadrao() {
+		return dataPadrao;
+	}
+
+	public void setDataPadrao(Date dataPadrao) {
+		this.dataPadrao = dataPadrao;
+	}
+}

1affa07283ccedc35d281b645776cf9257680d42
= <ryaninternet62@gmail.com>
2023-01-01 14:02:54 -0300

    Adicionei os principais eventos para cronograma de eventos.

--- Cards/src/main/java/br/com/ryan/planilha/PlanilhaManager.java
+++ Cards/src/main/java/br/com/ryan/planilha/PlanilhaManager.java
@@ -1,13 +1,20 @@
 package br.com.ryan.planilha;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
+import org.primefaces.event.ScheduleEntryMoveEvent;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.DefaultScheduleEvent;
 import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
 import org.primefaces.model.ScheduleModel;
 
 @ManagedBean @ViewScoped
@@ -20,6 +27,24 @@ public class PlanilhaManager implements Serializable {
     @PostConstruct
     public void init() {
         eventModel = new DefaultScheduleModel();
+    }
+    
+    public void onDateSelect(SelectEvent<LocalDateTime> selectEvent) {
+    	eventModel = (ScheduleModel) DefaultScheduleEvent.builder()
+                .startDate(selectEvent.getObject())
+                .endDate(selectEvent.getObject().plusHours(1))
+                .build();
+    }
+    
+    public void onEventSelect(SelectEvent<ScheduleEvent<?>> selectEvent) {
+    	eventModel = (ScheduleModel) selectEvent.getObject();
+    }
+    
+    public void onEventMove(ScheduleEntryMoveEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved",
+                "Delta:" + event.getDeltaAsDuration());
+
+        FacesContext.getCurrentInstance().addMessage(null, message);
     }
     
 	public ScheduleModel getEventModel() {

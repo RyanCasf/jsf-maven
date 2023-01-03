6cd5078c193c66d2f0012a38b453d9e81f1b10e7
= <ryaninternet62@gmail.com>
2023-01-02 23:41:40 -0300

    Iniciei o desenvolvimento da aba de 'Chronoline'.

---
+++ Cards/src/main/java/br/com/ryan/chronoline/ChronolineManager.java
@@ -0,0 +1,11 @@
+package br.com.ryan.chronoline;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean @ViewScoped
+public class ChronolineManager implements Serializable {
+		
+}

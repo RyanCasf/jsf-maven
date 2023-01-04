d31aa1fc897ea7bd9034b5f83e5d2af293413d78
= <ryaninternet62@gmail.com>
2023-01-03 22:26:04 -0300

    Adicionei a aba de chip.

--- Cards/src/main/java/br/com/ryan/chronoline/ChronolineManager.java
+++ Cards/src/main/java/br/com/ryan/chronoline/ChronolineManager.java
@@ -7,5 +7,6 @@ import javax.faces.bean.ViewScoped;
 
 @ManagedBean @ViewScoped
 public class ChronolineManager implements Serializable {
-		
+	private static final long serialVersionUID = -3970756048179091676L;
+	
 }

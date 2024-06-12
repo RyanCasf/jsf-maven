5dbd476d039c3750fbaec3c5a9dc79d514605685
= <ryaninternet62@gmail.com>
2024-06-12 05:08:17 -0300

    Descontinuação dos projetos completos.

--- Cards/src/test/java/br/com/ryan/util/ClasseConstrutorPadrao.java
+++
@@ -1,8 +0,0 @@
-package br.com.ryan.util;
-
-public class ClasseConstrutorPadrao {
-	
-	public ClasseConstrutorPadrao() {
-		
-	}
-}
--- Cards/src/test/java/br/com/ryan/util/ClasseSemConstrutorPadrao.java
+++
@@ -1,8 +0,0 @@
-package br.com.ryan.util;
-
-public class ClasseSemConstrutorPadrao {
-	
-	public ClasseSemConstrutorPadrao(String... msg) {
-		super();
-	}
-}

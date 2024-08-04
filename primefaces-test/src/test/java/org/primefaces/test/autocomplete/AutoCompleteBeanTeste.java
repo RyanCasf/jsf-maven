6b7bf9532626fbd9d50fb9fc87f33a86d4b84054
= <ryaninternet62@gmail.com>
2024-08-04 09:25:20 -0300

    Reset para autocomplete.

--- primefaces-test/src/test/java/org/primefaces/test/autocomplete/AutoCompleteBeanTeste.java
+++ primefaces-test/src/test/java/org/primefaces/test/autocomplete/AutoCompleteBeanTeste.java
@@ -1,24 +1,28 @@
 package org.primefaces.test.autocomplete;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.primefaces.event.SelectEvent;
 import org.primefaces.test.util.ParameterizedNameDefaultTest;
 
 @DisplayName("AutoComplete Bean")
-@ExtendWith(MockitoExtension.class)
 class AutoCompleteBeanTeste {
 	
-	@InjectMocks
 	private AutoCompleteBean bean;
+	
+	@BeforeEach
+	void setUp() {
+		bean = new AutoCompleteBean();
+		bean.setTexto("Texto");
+	}
 	
 	@ParameterizedNameDefaultTest
 	@NullAndEmptySource
@@ -26,6 +30,7 @@ class AutoCompleteBeanTeste {
 	@DisplayName("Complete vazio.")
 	void completeVazio(String value) {
 		assertTrue(bean.complete(value).isEmpty());
+		assertNull(bean.getTexto());
 	}
 	
 	@ParameterizedNameDefaultTest
@@ -33,5 +38,12 @@ class AutoCompleteBeanTeste {
 	@DisplayName("Complete.")
 	void complete(String value) {
 		assertEquals(Arrays.asList("0", "1", "2", value), bean.complete(value));
+	}
+	
+	@ParameterizedTest(name = "Evento nulo.")
+	@NullSource
+	@DisplayName("ItemSelect com evento nulo.")
+	void itemSelectComEventoNulo(SelectEvent	evento) {
+		assertDoesNotThrow(() -> bean.itemSelect(evento));
 	}
 }
